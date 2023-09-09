9591983647ca72d5626112b6895ed81536c2a781
benjyalper <benjyalper@hotmail.com>
2023-09-09 23:18:18 +0300

    initial

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 let selectedImage = null;
+let selectedSquare = null;
 
-$('.square').on('click', function() {
-    const square = $(this).closest('.square');
-    // $(square).css('margin', '5px');
-    $(square).css('border', '5px solid yellow');
-    // $(square).css('background-color', 'black');
-});
+
+// $('.square').on('click', function() {
+//     const square = $(this).closest('.square');
+//     // $(square).css('margin', '5px');
+//     $(square).css('border', '5px solid yellow');
+//     // $(square).css('background-color', 'black');
+// });
 
 $('img').on('click', function() {
-    const clickedImg = $(this.closest('img'));
-    if (selectedImage === null) {
-        selectedImage = clickedImg;
+    selectedImage = $(this.closest('img'));
         // selectedImage = $(this).closest('img');
-    } else {
-        // This is the second click, move the selected image to the clicked square
-        const targetSquare = $(this).closest('.square');
-        targetSquare.empty().append(selectedImage);
-        selectedImage = null; // Reset the selected image
+});
+
+$('.square').on('click', function(){
+    if (selectedImage !== null) {
+        selectedSquare = this.closest('.square');
+        $(selectedSquare).append(selectedImage);
     }
+    // selectedImage = null;
 });
